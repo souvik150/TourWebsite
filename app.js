3325f885a72303b44169f24b2d6211cec9363862
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-20 23:14:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
+const fs = require('fs');
 const express = require("express");
 const app = express();
 
-console.log("Hello This is the new 6.10 version")
+// app.get('/', (req, res) => {
+//     // res.status(200).send('Hello from the server side')
+//     res.status(200).json({message: 'Hello from the server side', app: 'Natours'})
+// });
+//
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint.')
+// });
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req,res) => {
+    res.status(200).json({
+        status: 'Success',
+        data: {
+            tours: tours,
+        }
+    })
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+});
+
