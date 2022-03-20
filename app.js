06715c9054a93d1d860964d3f85c5e6bb313f8ab
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-21 02:11:40 +0530

    20-03

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     // res.status(200).send('Hello from the server side')
 //     res.status(200).json({message: 'Hello from the server side', app: 'Natours'})
@@ -12,17 +14,34 @@ const app = express();
 // });
 
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/dev-data/tours-simple.json`)
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 app.get('/api/v1/tours', (req,res) => {
     res.status(200).json({
         status: 'Success',
+        results: tours.length,
         data: {
             tours: tours,
         }
     })
 })
+
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours[tours.length - 1].id+ 1;
+    const newTour = Object.assign({
+        id: newID},req.body)
+
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours),err => {
+        res.status(201).json({
+            status:'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
+;})
 
 const port = 3000;
 app.listen(port, () => {
