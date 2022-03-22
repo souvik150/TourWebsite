d295640264d24536b9546046f94d225afb34c790
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-23 02:48:11 +0530

    Finishing version 6

--- app.js
+++ app.js
@@ -1,34 +1,31 @@
-const fs = require('fs');
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require("./routes/tourRoutes")
-const userRouter = require("./routes/userRoutes")
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
-//1) Middlewares
-app.use(morgan('dev'))
-
-// .use is used to use middleware
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log("Hello from the middleware ");
-    next();
-})
+  console.log('Hello from the middleware 👋');
+  next();
+});
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString();
-    next();
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-// 3) Routes
+// 3) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 module.exports = app;
-
--- server.js
+++ server.js
@@ -1,12 +1,9 @@
 const dotenv = require('dotenv');
 const app = require('./app');
 
-dotenv.config({path: './config.env'});
+dotenv.config({ path: './config.env' });
 
-// console.log(app.get('env'));
-console.log(process.env);
-
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}...`)
+  console.log(`App running on port ${port}...`);
 });
