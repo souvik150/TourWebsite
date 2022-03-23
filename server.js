dda75a868e3a64fc82fdd921530cfcd79d53656f
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-23 16:09:11 +0530

    Final

--- server.js
+++ server.js
@@ -1,7 +1,59 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const db = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true
+  })
+  .then(con => console.log('DB connection established'));
+
+//---------------------------------------------------
+// Creating simple tour model
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker2',
+  rating: 4.7,
+  price: 497
+});
+
+testTour
+  .save()
+  .then(doc => {
+    console.log(doc);
+  })
+  .catch(err => {
+    console.log('ERROR:🧨', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
