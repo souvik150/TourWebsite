eb21aa0c4237231d1019bf6d4825525507babca1
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-23 16:47:39 +0530

    final

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,17 +3,17 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
-router.param('id', tourController.checkID);
+// router.param('id', tourController.checkID);
 
 router
-    .route('/')
-    .get(tourController.getAllTours)
-    .post(tourController.checkBody, tourController.createTour);
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.checkBody, tourController.createTour);
 
 router
-    .route('/:id')
-    .get(tourController.getTour)
-    .patch(tourController.updateTour)
-    .delete(tourController.deleteTour);
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
