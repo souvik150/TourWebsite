a3fb7882c71f32e4b7e27f4a109cba7fa1411f24
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-23 16:37:58 +0530

    start

--- server.js
+++ server.js
@@ -19,42 +19,6 @@ mongoose
   })
   .then(con => console.log('DB connection established'));
 
-//---------------------------------------------------
-// Creating simple tour model
-
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true
-  },
-  rating: {
-    type: Number,
-    default: 4.5
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price']
-  }
-});
-
-const Tour = mongoose.model('Tour', tourSchema);
-
-const testTour = new Tour({
-  name: 'The Forest Hiker2',
-  rating: 4.7,
-  price: 497
-});
-
-testTour
-  .save()
-  .then(doc => {
-    console.log(doc);
-  })
-  .catch(err => {
-    console.log('ERROR:🧨', err);
-  });
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
