e170f80dd7bda1ccdcb266d09a8959b83b29fba4
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-31 17:03:37 +0530

    y

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,14 @@ const userController = require('./../controllers/userController');
 const router = express.Router();
 
 router
-    .route('/')
-    .get(userController.getAllUsers)
-    .post(userController.createUser);
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
-    .route('/:id')
-    .get(userController.getUser)
-    .patch(userController.updateUser)
-    .delete(userController.deleteUser);
+  .route('/:id')
+  .get(userController.getUser)
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
