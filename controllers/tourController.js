450efdddfe1db4180dd2592dbce3d3d2de3071f1
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-03-31 21:24:14 +0530

    done

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,6 +3,13 @@
 /* eslint-disable no-console */
 const Tour = require('./../models/tourModel');
 
+exports.aliasTopTours = async (req, res, next) => {
+  req.query.limit = '5';
+  req.query.sort = '-ratingsAverage,price';
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+  next();
+};
+
 exports.getAllTours = async (req, res) => {
   try {
     //BUILD THE QUERY
