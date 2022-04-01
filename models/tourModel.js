bee957b0db1a1101701e917480f2c055abd76a44
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-04-01 16:19:40 +0530

    Update tourModel.js

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,13 +1,18 @@
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
   {
     name: {
       type: String,
       required: [true, 'A tour must have a name'],
-      unique: true
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters']
+      // validate: [validator.isAlpha, 'A tour name must only contain characters']
     },
     slug: String,
     duration: {
@@ -20,7 +25,11 @@ const tourSchema = new mongoose.Schema(
     },
     difficulty: {
       type: String,
-      required: [true, 'A tour must have a difficulty']
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium or difficult'
+      }
     },
     rating: {
       type: Number,
@@ -28,7 +37,9 @@ const tourSchema = new mongoose.Schema(
     },
     ratingsAverage: {
       type: Number,
-      default: 4.5
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0']
     },
     ratingsQuantity: {
       type: Number,
@@ -38,7 +49,17 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function(val) {
+          // this only points to current doc on NEW document creation
+          return val < this.price;
+        },
+        message: 'Discount price ({VALUE}) should be below regular price'
+      }
+    },
+
     summary: {
       type: String,
       trim: true,
@@ -71,6 +92,7 @@ tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
 });
 
+//==================================================//
 //Document middleware: runs before .save() and .create()
 //Below is a pre save hook
 tourSchema.pre('save', function(next) {
@@ -98,8 +120,16 @@ tourSchema.pre('/^find/', function(next) {
 });
 
 tourSchema.pre(/^find/, function(docs, next) {
-  console.log(docs);
+  // console.log(docs);
   console.log(`Query took ${Date.now() - this.start} milliseconds!`);
+  next();
+});
+
+//====================================================//
+//Aggregation middleware: runs before .aggregate()
+tourSchema.pre('aggregate', function(next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
   next();
 });
 
