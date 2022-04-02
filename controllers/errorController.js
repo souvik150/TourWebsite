f68f45ffde6493c40fc4b7b25ea8a92622325c54
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-04-02 13:50:48 +0530

    Update errorController.js

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,11 +1,43 @@
+/* eslint-disable no-console */
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      error: {}
+    });
+
+    //Programming or other unknown error: don't leak error details
+  } else {
+    //1) Log error
+    console.error('ERROR 💥', err);
+    //2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong'
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   // console.log(err.stack);
 
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (!process.env.NODE_ENV !== 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
