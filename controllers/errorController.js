9778faf833e801e52e5e364eefa84d15cf95e686
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-04-02 15:13:07 +0530

    y

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,9 +1,30 @@
 /* eslint-disable no-console */
+const AppError = require('./../utlis/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldsDB = err => {
+  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  console.log(value);
+
+  const message = `Duplicate field value: ${value}. Please use another value!`;
+  return new AppError(message, 400);
+};
+const handleValidationErrorDB = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
+    error: err,
     message: err.message,
-    error: err.message,
     stack: err.stack
   });
 };
@@ -13,18 +34,18 @@ const sendErrorProd = (err, res) => {
   if (err.isOperational) {
     res.status(err.statusCode).json({
       status: err.status,
-      message: err.message,
-      error: {}
+      message: err.message
     });
 
-    //Programming or other unknown error: don't leak error details
+    // Programming or other unknown error: don't leak error details
   } else {
-    //1) Log error
+    // 1) Log error
     console.error('ERROR 💥', err);
-    //2) Send generic message
+
+    // 2) Send generic message
     res.status(500).json({
       status: 'error',
-      message: 'Something went wrong'
+      message: 'Something went very wrong!'
     });
   }
 };
@@ -35,9 +56,16 @@ module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  if (!process.env.NODE_ENV !== 'development') {
+  if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = { ...err };
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handleValidationErrorDB(error);
+
+    sendErrorProd(error, res);
   }
 };
--- server.js
+++ server.js
@@ -4,8 +4,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env' });
+process.on('uncaughtException', err => {
+  console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -24,4 +29,12 @@ mongoose
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
