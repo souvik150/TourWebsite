60918e1c1a7e8fed7c6f7f3c63f489e3ef0b0535
Souvik Mukherjee <souvikmukherjee150@gmail.com>
2022-04-04 17:22:29 +0530

    y

--- controllers/authController.js
+++ controllers/authController.js
@@ -114,6 +114,29 @@ exports.restrictTo = (...roles) => {
         new AppError('You do not have permission to perform this action', 403)
       );
     }
+
     next();
   };
 };
+
+exports.forgotPassword = catchAsync(async (req, res, next) => {
+  // 1) Get user based on POSTed email
+
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new AppError('There is no user with email address.', 404));
+  }
+
+  //2) Generate the random reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({
+    validateBeforeSave: false
+  });
+
+  //3) Set the reset token to user
+
+  //4) Send it to user's email
+});
+
+exports.resetPassword = catchAsync(async (req, res, next) => {});
